841178b1dacb297db19bb8e79a427cccda674a4e
Łukasz <130372112+Grylix22@users.noreply.github.com>
2023-09-11 20:14:45 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+// slider move carousel time control
+const carouselWrapp = document.querySelector('#main-carousel')
+let carousel = new bootstrap.Carousel(carouselWrapp, {
+    interval: 2000,
+    wrap: true,
+    touch: true
+})
+const playBtn = document.querySelector('#playBtn');
+let isPlaying = true;
+carousel.cycle();
+
+
+// slider buttons control
+
+// pause 
+function sliderPause() {
+    if (isPlaying == false) {
+        isPlaying = true;
+        playBtn.classList.toggle("active");
+        carousel.next();
+        carousel.cycle();
+    } else {
+        isPlaying = false;
+        playBtn.classList.toggle("active");
+        carousel.pause();
+    }
+}
